301da98c4fc501242fd9aa2447cef17306df0965
Tenniel <tenniel.lynn@gmail.com>
2024-04-14 07:42:50 -0700

    deploy db backend to prod

--- index.js
+++ index.js
@@ -31,10 +31,10 @@ let notes = [
 
   const cors = require('cors')
 
+  app.use(express.static('dist'))
   app.use(express.json())
   app.use(requestLogger)
   app.use(cors())
-  app.use(express.static('dist'))
 
   const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
@@ -44,28 +44,31 @@ let notes = [
     response.send('<h1>Hello World!</h1>')
   }) 
   
-  app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
+  app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
         response.json(note)
-    } else {
+      } else {
         response.status(404).end()
-    }
+      }
+    })
+    .catch(error => next(error))
   })
 
   app.get('/api/notes/', (request, response) => {
+    console.log(Note)
     Note.find({}).then(notes => {
         response.json(notes)
     })
   })
 
-  app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end()
+  app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
   })
 
   const generateId = () => {
@@ -75,13 +78,8 @@ let notes = [
 
     return maxId+1
   }
-  app.post('/api/notes', (request, response) => {
+  app.post('/api/notes', (request, response, next) => {
     const body = request.body
-    if (body.content === undefined) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
 
     const note = new Note ({
         content: body.content,
@@ -91,9 +89,37 @@ let notes = [
     note.save().then(savedNote => {
         response.json(savedNote)
     })
+    .catch(error => next(error))
+  })
+
+  app.put('/api/notes/:id', (request, response, next) => {
+    const {content, important} = request.body
+  
+    Note.findByIdAndUpdate(request.params.id,
+      {content, important},
+      { new: true, runValidators: true, context: 'query' })
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
   })
   
   app.use(unknownEndpoint) // how to put the middleware to use
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+      return response.status(400).json({error: error.message})
+    }
+  
+    next(error)
+  }
+  
+  // this has to be the last loaded middleware, also all the routes should be registered before this!
+  app.use(errorHandler)
 
   const PORT = process.env.PORT || process.env.PORT
   app.listen(PORT, () => {
